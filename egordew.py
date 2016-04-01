504ca798529d89d99734ef6d5bd0b325324c417a
snowmirage <be2rit@gmail.com>
2016-04-01 14:02:43 -0400

    created 3 scripts for temp humidity and dew point also renamed egorpower to egorwatts these should all reside in the check_mk local checks directory on the server that has the temp hum and cyberpower ups attached

---
+++ egordew.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Pull the temp in F, realative humid, and dew point and send to check_mk
+import os
+import re
+
+output = os.popen("sudo /home/dev@egor.betoria.dyndns.org/temphumid/TEMPered/build/utils/tempered -s Fahrenheit").read()
+output2 = re.compile('\S+').findall(output)
+
+#print output
+#print output2
+#print output2[10]
+
+print "0 Egordew dewpoint=%s;80;70;0;200 OK - Dew Point in the Garage" % output2[10]
+
