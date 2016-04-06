b4b55fa01495255c041dae4973000b12b39835a8
snowmirage <be2rit@gmail.com>
2016-04-06 12:57:39 -0400

    updated freenastemp.py now works, also converted to F

--- freenastemp.py
+++ freenastemp.py
@@ -4,7 +4,7 @@ import subprocess
 import sys
 
 host = "192.168.0.40"
-keyfile = "/home/dev@egor.betoria.dyndns.org/powerpc_id_rsa"
+keyfile = "/home/powerpc/.ssh/id_rsa"
 command = "sysctl -a | egrep -E \"cpu\.[0-9]+\.temp\""
 
 # ssh part
@@ -42,7 +42,15 @@ else:
 		#print t[1].replace("C", "")
 		cputemps.append(t[1].replace("C", ""))
 
+        cputempsF = []
+        for x in cputemps:
+                cputempsF.append(9.0/5.0*float(x)+32)
+
+#        print cputemps
+#        print cputempsF
+
 #print cputemps
 
-print "0 FreenasTemp core0=%s;85;95;0;100|core1=%s;85;95;0;100|core2=%s;85;95;0;100|core3=%s;85;95;0;100|core4=%s;85;95;0;100|core5=%s;85;95;0;100|core6=%s;85;95;0;100|core7=%s;85;95;0;100 OK - Temp of all 8 cores on freenas" % (cputemps[0], cputemps[1], cputemps[2], cputemps[3], cputemps[4], cputemps[5], cputemps[6], cputemps[7])
+print "0 FreenasTemp core0=%s;85;95;0;100|core1=%s;85;95;0;100|core2=%s;85;95;0;100|core3=%s;85;95;0;100|core4=%s;85;95;0;100|core5=%s;85;95;0;100|core6=%s;85;95;0;100|core7=%s;85;95;0;100 OK - Temp of all 8 cores on freenas" % (cputempsF[0], cputempsF[1], cputempsF[2], cputempsF[3], cputempsF[4], cputempsF[5], cputempsF[6], cputempsF[7])
 
+# the max temp for this cpu is 76C   or 168.8F adjust the crit / warn to match that sometime soon
